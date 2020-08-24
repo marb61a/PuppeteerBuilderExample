a3d9a13a0dc6f23c93edf5d58646d817a0c1b1f2
Martin <marb61a@gmail.com>
2020-08-24 20:10:38 +0100

    Added configurations and tested

---
+++ build/tests/example.test.js
@@ -0,0 +1,7 @@
+"use strict";
+
+describe("Config Test", function () {
+    it("should work okay", function () {
+        console.log("Hooray");
+    });
+});
---
+++ src/tests/example.test.js
@@ -0,0 +1,5 @@
+describe("Config Test", () => {
+    it("should work okay", () => {
+        console.log("Hooray");
+    })
+})
